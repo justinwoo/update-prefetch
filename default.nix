65fcd0d7907c0c4491bdaabf2bc1ef3a59ae1767
justinwoo <moomoowoo@gmail.com>
2019-10-01 14:44:03 -0400

    run nixpkgs-fmt if exists in PATH

--- default.nix
+++ default.nix
@@ -5,21 +5,21 @@ let
     inherit pkgs;
   };
 
-in pkgs.runCommand "update-prefetch" {
+in
+pkgs.runCommand "update-prefetch" {
   name = "update-prefetch";
 
   buildInputs = [ pkgs.makeWrapper ];
 } ''
-    target=$out/bin/update-prefetch
-    mkdir -p $out/bin
+  target=$out/bin/update-prefetch
+  mkdir -p $out/bin
 
-    ln -s ${n2n.package.outPath}/bin/update-prefetch $target
+  ln -s ${n2n.package.outPath}/bin/update-prefetch $target
 
-    wrapProgram $target --prefix PATH : ${
-      pkgs.lib.makeBinPath [
-        n2n.package
-        pkgs.nix-prefetch-git
-        pkgs.nix
-      ]
-    }
-  ''
+  wrapProgram $target --prefix PATH : ${pkgs.lib.makeBinPath [
+  n2n.package
+  pkgs.nix-prefetch-git
+  pkgs.nix
+  pkgs.which
+]}
+''
--- src/Main.purs
+++ src/Main.purs
@@ -5,14 +5,18 @@ import Prelude
 import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
+import Data.Monoid (guard)
 import Data.Traversable (for, traverse)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (error)
 import FormatNix (TreeSitterParser, children, mkParser, nixLanguage, parse, printExpr, readNode, rootNode)
+import Node.ChildProcess (Exit(..))
+import Node.ChildProcess as CP
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile, writeTextFile)
+import Sunde as S
 import Update (updateFetchAttrs)
 
 foreign import argv :: Array String
@@ -43,6 +47,17 @@ runUpdate fileName = do
       let output = Array.intercalate "\n" $ printExpr <$> nodes'
 
       writeTextFile UTF8 fileName output
+
+      let cmd = "nixpkgs-fmt"
+      checkNixpkgsFmt <- S.spawn { cmd: "which", args: [cmd], stdin: Nothing } CP.defaultSpawnOptions
+      case checkNixpkgsFmt.exit of
+        Normally 0 -> do
+          let args = [fileName]
+          out <- S.spawn { cmd, args, stdin: Nothing } CP.defaultSpawnOptions
+          error "ran nixpkgs-fmt:"
+          error out.stderr
+        _ -> pure unit
+
       error $ "updated " <> fileName <> "."
     Left _ -> do
       error "encountered errors in parsing input file."
